e7118c2c3c5de4b342170b9e16214cdffa069eff
Jorge Sosa <jorgesosa@fundaciondevalores.org>
2024-09-26 11:18:02 -0400

    remove unnecesary comments

--- src/index.js
+++ src/index.js
@@ -8,13 +8,6 @@ import { LogError, ErrorHandler } from "./middlewares/ErrorsHandler.js";
 
 const swaggerDocument = JSON.parse(fs.readFileSync("./swagger-output.json", "utf8"));
 
-// const router = require('express').Router();
-// const swaggerUi = require('swagger-ui-express');
-// const swaggerDocument = require('./swagger.json');
-
-// router.use('/api-docs', swaggerUi.serve);
-// router.get('/api-docs', swaggerUi.setup(swaggerDocument));
-
 const app = express();
 
 app.use(express.json());
