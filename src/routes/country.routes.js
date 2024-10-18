d2e53704c24191695d8f52d12008827ac481355e
Jorge Sosa <jorgesosa@fundaciondevalores.org>
2024-10-18 14:09:42 -0400

    migrate contry to pg

--- src/routes/country.routes.js
+++ src/routes/country.routes.js
@@ -1,8 +1,11 @@
-import { all } from "../controllers/country.controller.js";
+import { create, index, update, show } from "../controllers/country.controller.js";
 import { Router } from "express";
 
 const country_router = Router();
-
-country_router.get('/', all);
+ 
+country_router.get('/', index);
+country_router.get('/:id', show);
+country_router.post('/', create);
+country_router.put('/:id', update);
 
 export default country_router;
