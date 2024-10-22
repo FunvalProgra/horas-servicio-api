2261222bbd2eaf55b15ef1f79072bb3bffec6974
Harold Carazas <car23068@byui.edu>
2024-10-22 11:46:10 -0500

    NODE_ENV added

--- src/libs/swagger.js
+++ src/libs/swagger.js
@@ -1,11 +1,14 @@
 import swaggerAutogen from 'swagger-autogen';
 import { APP_URL } from '../config/app.config.js';
 
+const scheme = process.env.NODE_ENV === 'production' ? 'https' : 'http';
+
 const doc = {
   info: {
     title: 'Horas de Servicio API',
     description: 'Demo API para curso de React y Node.js',
   },
+  schemes: [scheme],
   host: `${APP_URL}/api/v1`
 };
 
